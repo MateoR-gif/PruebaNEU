4f46db17b42bec951dbc2b7a86d65283f617d04f
MateoR-gif <mateorendon008@gmail.com>
2022-11-30 14:57:33 -0500

    Front-End Inconcluso

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  var data = JSON.stringify({
+    "collection": "Sub-Arrays",
+    "database": "NEUTest",
+    "dataSource": "NEUCluster",
+    "projection": {
+        "_id": 1
+    }
+});
+var config = {
+    method: 'post',
+    url: 'https://data.mongodb-api.com/app/data-lwvft/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'fLPItnpNimgOY7vAhs1j5Vg3OMDsY2F9qzxlucX5Ha3Q9PaqGKZdZKd7RWN3bf0E',
+    },
+    data: data
+};
+  const getData = async () => {
+    await axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prueba Técnica NEU
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
